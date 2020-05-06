be947c49a39eb24057fc30578b0bdb88c8886e54
Yash Malani <53264955+yash2806@users.noreply.github.com>
2020-05-06 18:33:11 +0530

    Create campusRecruitmentDatasetUsingDecisionTree.py

---
+++ campusRecruitmentDatasetUsingDecisionTree.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np 
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
+file_loc = '/home/tuesday/Dataset/Placement_Data_Full_Class.csv'
+df = pd.read_csv(file_loc)
+df.head()
+
+df.describe()
+
+df['salary']
+
+df['status'].replace("Placed",1,inplace=True)
+df['status'].replace("Not Placed",0,inplace=True)
+df['salary'].fillna(0,inplace=True)
+
+features = ['ssc_p','hsc_p','degree_p','etest_p','status','mba_p']
+X = df[features]
+y = df['salary']
+train_X,val_X,train_y,val_y = train_test_split(X,y,random_state=1)
+salary_model = DecisionTreeRegressor(random_state = 1)
+
+salary_model.fit(train_X,train_y)
+
+val_pred = salary_model.predict(val_X)
+mae = mean_absolute_error(val_pred,val_y)
+print("Mean Absolute Error is "+mae)
+
